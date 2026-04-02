ea6b13fa6231d4e96a9f45c5b1e6a296c360dbf4
malcomothieno <malcomothieno@gmail.com>
2026-04-02 12:24:31 +0300

    Update assignment2.sql

--- motors/database/assignment2.sql
+++ motors/database/assignment2.sql
@@ -1,3 +1,5 @@
+-- Query 1: Insert Tony Stark into the account table
+
 INSERT INTO account (
   account_firstname,
   account_lastname,
@@ -12,14 +14,19 @@ VALUES (
 );
 
 
+-- Query 2: Update Tony Stark's account_type to "Admin"
+
 UPDATE account
 SET account_type = 'Admin'
 WHERE account_email = 'tony@starkent.com';
 
 
+-- Query 3: Delete the Tony Stark record
+
 DELETE FROM account
 WHERE account_email = 'tony@starkent.com';
 
+-- Query 4: Update GM Hummer description
 
 UPDATE inventory
 SET inv_description = REPLACE(
@@ -29,7 +36,7 @@ SET inv_description = REPLACE(
 )
 WHERE inv_make = 'GM' AND inv_model = 'Hummer';
 
-
+-- Query 5: Inner Join – Sport category inventory
 
 SELECT
   inv.inv_make,
@@ -40,7 +47,7 @@ INNER JOIN classification cls
   ON inv.classification_id = cls.classification_id
 WHERE cls.classification_name = 'Sport';
 
-
+-- Query 6: Update inv_image and inv_thumbnail paths
 UPDATE inventory
 SET
   inv_image     = REPLACE(inv_image,     '/images/', '/images/vehicles/'),
