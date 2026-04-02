277309cff9d1b9e0f33dacf15a3b0eabd911c65d
malcomothieno <malcomothieno@gmail.com>
2026-04-02 14:38:24 +0300

    Update baseController.js

--- motors/controllers/baseController.js
+++ motors/controllers/baseController.js
@@ -1,8 +1,12 @@
+const utilities = require("../utilities")
+
 const baseController = {}
 
-baseController.buildHome = async function (req, res) {
+baseController.buildHome = async function (req, res, next) {
+  const nav = await utilities.getNav()
   res.render("index", {
     title: "CSE Motors | Home",
+    nav,
     vehicle: {
       name: "DMC Delorean",
       features: ["3 Cup holders", "Superman doors", "Fuzzy dice!"],
@@ -10,17 +14,17 @@ baseController.buildHome = async function (req, res) {
       link: "/inv/detail/1",
     },
     upgrades: [
-      { name: "Flux Capacitor", image: "/images/upgrades/flux-capacitor.jpg", link: "/inv/upgrade/flux-capacitor" },
-      { name: "Flame Decals",   image: "/images/upgrades/flame-decals.jpg",   link: "/inv/upgrade/flame-decals" },
-      { name: "Bumper Stickers",image: "/images/upgrades/bumper-stickers.jpg",link: "/inv/upgrade/bumper-stickers" },
-      { name: "Hub Caps",       image: "/images/upgrades/hub-caps.jpg",       link: "/inv/upgrade/hub-caps" },
+      { name: "Flux Capacitor",  image: "/images/upgrades/flux-capacitor.jpg",  link: "/inv/upgrade/flux-capacitor" },
+      { name: "Flame Decals",    image: "/images/upgrades/flame-decals.jpg",    link: "/inv/upgrade/flame-decals" },
+      { name: "Bumper Stickers", image: "/images/upgrades/bumper-stickers.jpg", link: "/inv/upgrade/bumper-stickers" },
+      { name: "Hub Caps",        image: "/images/upgrades/hub-caps.jpg",        link: "/inv/upgrade/hub-caps" },
     ],
     reviews: [
       { text: "So fast it's almost like traveling in time.", rating: "4/5" },
-      { text: "Coolest ride on the road.",                  rating: "4/5" },
-      { text: "I'm feeling McFly!",                        rating: "5/5" },
-      { text: "The most futuristic ride of our day.",       rating: "4.5/5" },
-      { text: "80's livin and I love it!",                  rating: "5/5" },
+      { text: "Coolest ride on the road.",                   rating: "4/5" },
+      { text: "I'm feeling McFly!",                         rating: "5/5" },
+      { text: "The most futuristic ride of our day.",        rating: "4.5/5" },
+      { text: "80's livin and I love it!",                   rating: "5/5" },
     ],
   })
 }
