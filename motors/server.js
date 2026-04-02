6d481d7791e08c75b428983df3bfec2d64e047a4
malcomothieno <malcomothieno@gmail.com>
2026-04-02 14:37:45 +0300

    Update server.js

--- motors/server.js
+++ motors/server.js
@@ -1,45 +1,58 @@
-/* ***********************
- * Require Statements
- *************************/
+
+require("dotenv").config()
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const path = require("path")
+const utilities = require("./utilities")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 const app = express()
 
-/* ***********************
- * View Engine and Templates
- *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
 app.set("views", path.join(__dirname, "views"))
 
-/* ***********************
- * Static Files
- *************************/
+
 app.use(express.static(path.join(__dirname, "public")))
 
-/* ***********************
- * Routes
- *************************/
-const indexRouter = require("./routes/index")
-app.use("/", indexRouter)
 
-/* ***********************
- * 404 Handler
- *************************/
-app.use((req, res) => {
-  res.status(404).render("errors/404", {
-    title: "404 - Page Not Found",
-    nav: "",
+// Home route
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
+
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." })
+})
+
+
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+
+  const status = err.status || 500
+  let message
+
+  if (status === 404) {
+    message = err.message || "Page not found."
+  } else {
+    message =
+      "Oh no! There was a crash. Maybe try a different route? Our team has been notified."
+  }
+
+  res.status(status).render("errors/error", {
+    title: status + " Error",
+    nav,
+    message,
+    status,
   })
 })
 
-/* ***********************
- * Local Server Info
- * Values from .env (see .env file)
- *************************/
+
 const port = process.env.PORT || 5500
 const host = process.env.HOST || "localhost"
 
